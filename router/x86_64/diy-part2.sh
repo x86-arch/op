5277a336494fe1dece08e9db0c3ccc064633a605
ophub <68696949+ophub@users.noreply.github.com>
2020-11-16 22:21:24 +0800

    Update diy-part2.sh

--- router/x86_64/diy-part2.sh
+++ router/x86_64/diy-part2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #========================================================================================================================
 # https://github.com/ophub/op
-# Description: Automatically Build OpenWrt for x64
+# Description: Automatically Build OpenWrt for X86_64
 # Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
 # Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
 # Copyright (C) 2020 https://github.com/ophub/op
