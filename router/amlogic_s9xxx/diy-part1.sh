1cbfb8a326c72935ff44ea2f6146330177a02271
ophub <chinaue@gmail.com>
2021-04-18 14:13:37 +0800

    update

--- router/amlogic_s9xxx/diy-part1.sh
+++ router/amlogic_s9xxx/diy-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
+# https://github.com/ophub/op
 # Description: Automatically Build OpenWrt for Amlogic S9xxx STB
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
 # Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
