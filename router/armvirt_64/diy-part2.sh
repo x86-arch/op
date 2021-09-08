3ed9b39c77c16594aa6e349c3272ea378f1b28b2
x86-arch <51865943+x86-arch@users.noreply.github.com>
2021-09-08 20:34:56 +0700

    Update diy-part2.sh

--- router/armvirt_64/diy-part2.sh
+++ router/armvirt_64/diy-part2.sh
@@ -50,6 +50,61 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
 # sed -i "/DEFAULT_PACKAGES/ s/$/ pirania-app pirania ip6tables-mod-nat ipset shared-state-pirania uhttpd-mod-lua/" target/linux/armvirt/Makefile
 
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/dns2socks package/dns2socks
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria package/hysteria
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ipt2socks package/ipt2socks
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/kcptun package/kcptun
+
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks package/microsocks
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/naiveproxy package/naiveproxy
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/pdnsd-alt package/pdnsd-alt
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocks-rust package/shadowsocks-rust
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/shadowsocksr-libev package/shadowsocksr-libev
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/simple-obfs package/simple-obfs
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/trojan-plus
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan package/trojan
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-core package/v2ray-core
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/v2ray-plugin package/v2ray-plugin
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/xray-core
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-plugin package/xray-plugin
+
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash/ package/luci-app-openclash
+
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwal
+
+svn co https://github.com/x86-arch/NueXini_Packages/trunk/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+svn co https://github.com/x86-arch/NueXini_Packages/trunk/luci-theme-netgear package/luci-theme-netgear
+svn co https://github.com/x86-arch/NueXini_Packages/trunk/luci-theme-rosy package/luci-theme-rosy
+svn co https://github.com/giaulo/luci-app-filebrowser/trunk package/luci-app-filebrowser
+svn co https://github.com/lixingcong/dnsmasq-regex-openwrt/trunk package/dnsmasq-regex-openwrt
+
+sed -i "/DEFAULT_PACKAGES/ s/$/ coreutils python3-markdown coreutils-base64 coreutils-nohup curl dnsmasq-full dns2socks ipset ip-full ipt2socks iptables-mod-tproxy libuci-lua lua luci-lib-jsonc microsocks tcping resolveip unzip libpthread iptables bash ca-certificates iptables-mod-extra libcap ruby ruby-yaml/" target/linux/armvirt/Makefile
+
 # Apply patch
 # git apply ../router-config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
